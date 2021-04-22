bed5c2782fa95a4bb2d4ef13fbc78a7cec3e6249
Sarim Abbas <sarim.stays@gmail.com>
2021-04-22 18:55:32 -0400

    had to add the query selector

--- extension/options/options.js
+++ extension/options/options.js
@@ -1,15 +1,13 @@
 // https://developer.chrome.com/docs/extensions/mv2/options/
 
-document.addEventListener("blur", function (event) {
-  // chrome.extension
-  //   .getBackgroundPage()
-  //   .console.log("Someone typed in an iCal link");
+document
+  .querySelector("#ical-input")
+  .addEventListener("blur", function (event) {
+    const inputText = event.currentTarget.value;
 
-  const inputText = event.currentTarget.value;
-
-  // send the entered URL to background.js
-  chrome.runtime.sendMessage(
-    { message: "ical_feed_entered", payload: inputText },
-    function (response) {}
-  );
-});
+    // send the entered URL to background.js
+    chrome.runtime.sendMessage(
+      { message: "ical_feed_entered", payload: inputText },
+      function (response) {}
+    );
+  });
