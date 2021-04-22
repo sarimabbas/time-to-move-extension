5b61ce9e26b4d28167aa765d57002bca00888609
Sarim Abbas <sarim.stays@gmail.com>
2021-04-22 16:40:05 -0400

    add comments

--- extension/background.js
+++ extension/background.js
@@ -1,15 +1,23 @@
+// these are global variables that exist over the lifetime of the extension
+// (i.e. whenever Google Chrome is open)
 let user_signed_in = false;
 
+// the chrome runtime passes messages between the scripts in the frontend and this "backend" script
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  // you can ask if the user is signed in from the frontend with this
   if (request.message === "is_user_signed_in") {
     sendResponse({
       message: "success",
       payload: user_signed_in,
     });
-  } else if (request.message === "sign_out") {
+  }
+  // you can sign people out with this
+  else if (request.message === "sign_out") {
     user_signed_in = false;
     sendResponse({ message: "success" });
-  } else if (request.message === "sign_in") {
+  }
+  // this message is sent by the login.js upon successful sign in
+  else if (request.message === "sign_in") {
     user_signed_in = true;
     sendResponse({ message: "success" });
   }
