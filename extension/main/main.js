26fcb44ed14bc556eda26f915319f174e30ea5aa
jc3645 <jesse.chen@yale.edu>
2021-05-06 02:28:56 -0400

    for demo

--- extension/main/main.js
+++ extension/main/main.js
@@ -48,3 +48,20 @@ function init() {
 }
 
 init();
+
+var countDownDate = new Date("May 6, 2021 11:30:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("ppt").innerHTML =
+    hours + " : " + minutes + " : " + seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("ppt").innerHTML = "Breaktime!";
+  }
+}, 1000);
